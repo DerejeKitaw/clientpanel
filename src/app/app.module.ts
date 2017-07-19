efb161c678c3630410007868c8b67d739337ed70
mburkrb43 <mburkhart41@gmail.com>
2017-07-19 08:27:36 -0400

    AddClientform

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 //mhb added routing
 import { RouterModule, Routes } from '@angular/router';
@@ -31,7 +32,8 @@ import { ClientService } from './services/client.service';
 const appRoutes: Routes = [
   { path: '', component:DashboardComponent},
   { path: 'register', component:RegisterComponent},
-  { path: 'login', component:LoginComponent}
+  { path: 'login', component:LoginComponent},
+  { path: 'add-client', component:AddClientComponent}
 ];  
 
 export const firebaseConfig = {
@@ -59,6 +61,7 @@ export const firebaseConfig = {
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot(appRoutes),
     AngularFireModule.initializeApp(firebaseConfig) 
   ],
